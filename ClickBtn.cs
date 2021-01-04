129129988efb0d032c3be425bf8112d0e0ee81f7
shigotoyo12345678 <73812038+shigotoyo12345678@users.noreply.github.com>
2021-01-05 02:41:57 +0900

    Add files via upload

---
+++ ClickBtn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClickBtn : MonoBehaviour
+{
+    public Button facilityBtn;               //施設ボタン
+
+    //施設ボタン押下時処理
+    public void facilityBtnClick()
+    {
+
+        FacilityList.facilityNum = facilityBtn.name;
+
+        //施設リスト画面のシーンナンバーによって遷移先を変える
+        if (FacilityList.sceneNum == "1")
+        {
+            SceneManager.LoadScene("facilityDetail");
+        }
+        else if (FacilityList.sceneNum == "2")
+        {
+            SceneManager.LoadScene("upInput");
+        }
+
+    }
+
+
+}
